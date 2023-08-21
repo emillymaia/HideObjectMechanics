2e1c16673aed4117b5d8288934c59055aa065fa5
emillymaia <emillypereira1290@gmail.com>
2023-08-21 10:20:24 -0300

    Initial Commit

---
+++ HiddenObjectPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HiddenObjectPOC
+//
+//  Created by Emilly Maia on 21/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
